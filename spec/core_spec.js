13349ca1e5a4e7e3a44561398840b78d0e4f4760
Kristopher Chambers <kristopher.chambers@gmail.com>
2009-06-08 20:29:39 -0400

    Adding Smoke#argumentToArray. Turns an arguments object into an actual array.

--- spec/core_spec.js
+++ spec/core_spec.js
@@ -1,7 +1,7 @@
 Screw.Unit(function() {
 	describe("core", function() {    
+    var anonymous_function = function() { return arguments };
     describe("printArguments", function() {
-      var anonymous_function = function() { return arguments };
       it("should return '()' is the arguments are empty", function() {
         expect(Smoke.printArguments(arguments)).to(equal, '()');
       });
@@ -18,6 +18,21 @@ Screw.Unit(function() {
       it("should handle being passed something other than an array or arguments object", function() {
         expect(Smoke.printArguments(false)).to(equal, '(false)');
       });
-    });    
+    });
+    
+    describe("argumentsToArray", function() {
+      it("should return an array", function() {
+        expect(Smoke.argumentsToArray(anonymous_function(1,2)) instanceof Array).to(equal, true);
+      });
+      
+      it("should return the arguments in an array", function() {
+        expect(Smoke.argumentsToArray(anonymous_function(1,2))).to(equal, [1,2]);        
+      });
+      
+      it("should return the array if passed an array", function() {
+        var array = [];
+        expect(Smoke.argumentsToArray(array) === array).to(equal, true)
+      })
+    });
 	});
 });
